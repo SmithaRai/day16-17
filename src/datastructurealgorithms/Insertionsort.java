ee64113ab92f67dced8c9668fdb913ed373fb878
unknown <SmithaRai.com>
2022-04-24 16:58:44 +0530

    Added : added Algorithms

---
+++ src/datastructurealgorithms/Insertionsort.java
@@ -0,0 +1,23 @@
+package datastructurealgorithms;
+
+public class Insertionsort {
+	  public static void printArray(String[] arr) {
+	        for(int i =0; i<arr.length; i++) {
+	            System.out.print(arr[i]+" ");
+	        }
+	        System.out.println();
+	    }
+	    public static void main(String[] args) {
+	        String[] arr = {"Mango", "Grapes", "Apple"};
+	        for(int i =1; i<arr.length; i++) {
+	            String current = arr[i];
+	            int j = i-1;
+	            while(j >= 0 && arr[j].compareToIgnoreCase(current) > 0)  {
+	                arr[j+1] = arr[j];
+	                j--;
+	            }
+	            arr[j+1] = current;
+	        }
+	        printArray(arr);
+	    }
+}
